4d643c0da50f185a06eb45e4b96d70678bd255d4
KristyAcquah <akosuaacquah99@gmail.com>
2023-05-02 16:23:52 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - write char c to stdout 
+ * @c: to be printed
+ * Return: On success 1
+ */
+int _putchar(char c)
+{
+	int _putchar(char c);
+
+	return (write(1, &c, 1));
+}
