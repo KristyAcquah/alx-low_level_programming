6b88a899f0250d861f52cc7c72cf0e34c7452e8d
KristyAcquah <akosuaacquah99@gmail.com>
2023-05-03 16:09:05 -0700

    print reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - print reverse of a string
+ * @s: input
+ * Return: string in reverse
+ */
+void rev_string(char *s)
+{
+	char reverse = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		reverse = s[0];
+		s[i] = s[c];
+		s[c] = reverse;
+	}
+}
