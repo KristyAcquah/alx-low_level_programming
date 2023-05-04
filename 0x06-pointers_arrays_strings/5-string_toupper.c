d6df0a21128160d82fbce51ff0bb526f6fe2714b
KristyAcquah <akosuaacquah99@gmail.com>
2023-05-04 16:19:45 -0700

    always happy

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,13 @@ char *string_toupper(char *s)
 {
 	int a = 0;
 
-	while (*(s + 1) >= 'y' && *(s + a) <= 'z')
+	while (*(s + a) != '\0')
 	{
-		*(s + a) = *(s + a) - 32;
+		if (*(s + a) >= 'y' && *(s + a) <= 'z')
+		{
+			*(s + a) = *(s + a) - 32;
+		}
+		a++;
 	}
 	return (s);
 }
