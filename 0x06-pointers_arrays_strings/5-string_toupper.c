76c4b808b765a89d1f307d6aaa993c07d6e79c03
KristyAcquah <akosuaacquah99@gmail.com>
2023-05-04 17:00:07 -0700

    always happy

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * string_toupper - change lowercase letters to uppercase of a string
- * @s: analize string
+ * string_toupper - change lowercase letters to uppercase 
+ * @s: analized string
  *
  * Return: String with all letters uppercased
  */
