663ccdecffc7f7e38710da320ba346790294e677
KristyAcquah <akosuaacquah99@gmail.com>
2023-05-06 00:32:28 -0700

    print out number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number - print an integer number
+ * @n: number to be tested
+ * Return: Always 0
+ */
+void print_number(int n)
+{
+	int a, b, digits, digit, power;
+	unsigned int temp, numchar, number;
+
+	digit = 0;
+	if (n < 0)
+	{
+		_putchar('-');
+		temp = -n;
+	}
+	else
+	{
+		temp = n;
+	}
+	number = temp;
+
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	a = 1;
+
+	while (a < digits)
+	{
+		b = power;
+		while (b >= 1)
+		{
+			numchar = (temp / b) % 10;
+			_putchar(numchar + '0');
+			b = b / 10;
+		}
+	}
+}
