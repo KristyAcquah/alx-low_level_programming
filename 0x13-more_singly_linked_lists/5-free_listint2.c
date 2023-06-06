61ec5f95c1a52c3bcdfff41283a7fa8a14ea5b69
KristyAcquah <akosuaacquah99@gmail.com>
2023-06-06 12:33:49 -0700

    freed at last

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,12 +12,11 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (*head != NULL)
+	while (head != NULL)
 	{
-		current = (*head)->next;
-		*head = current;
+		current = *head;
+		*head = (*head)->next;
 		free(current);
 	}
 	*head = NULL;
 }
-
