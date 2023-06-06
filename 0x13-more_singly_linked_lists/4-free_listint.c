75593ae83411945fa617f11d79af27c74a7ba93b
KristyAcquah <akosuaacquah99@gmail.com>
2023-06-06 08:39:59 -0700

    free node

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,12 +7,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *now;
 
-	while (head == 0)
+	while (head == NULL)
 	{
-		temp = head->next;
+		now = head->next;
 		free(head);
-		head = temp;
+		head = now;
 	}
 }
