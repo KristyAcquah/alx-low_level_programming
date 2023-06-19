679e903c11506288b4b9528348c76eb4ee608147
KristyAcquah <akosuaacquah99@gmail.com>
2023-06-19 01:19:07 -0700

    singly

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,15 +10,14 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	const list_t *h = head;
 
-	while (head)
+	while (h)
 	{
-		if (!head->str)
+		if (!h->str)
 			printf("[0], (nil)\n");
 		else
-			printf("[%u] %s\n, head -> len, head -> str");
-		head = head->next;
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 		count++;
 	}
 	return (count);
