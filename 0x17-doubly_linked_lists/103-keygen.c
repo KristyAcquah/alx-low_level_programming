206bf39baebdb58757f1c847e89b558abeb32596
KristyAcquah <akosuaaqcuah99@gmail.com>
2023-08-26 19:23:27 -0700

    very tired

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - generate a ket depending on a username
+ * @argc: number of arguments passed
+ * @argv: arguments passed to main
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
